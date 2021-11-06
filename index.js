c1af8ad843e9531caf7804bb79bf1e286a82ee70
jessi68 <lynn312@unist.ac.kr>
2021-11-06 13:34:41 +0900

    refactor: index.js 로 코드 분리, 색깔 버튼 누르면 색깔 바뀌는 기능 함수로 만들고, 변수 이름 목적이 잘 드러나게 고침

---
+++ index.js
@@ -0,0 +1,111 @@
+var canvas, ctx, flag = false,
+prevX = 0,
+curX = 0,
+prevY = 0,
+curY = 0,
+dot_flag = false;
+
+var color = "black";
+lineWidth = 2;
+
+    function paintColor(colorButton) {
+        console.log(color);
+        console.log(colorButton);
+        color = colorButton.id;
+        console.log(color);
+        if (color == "white") lineWidth = 14;
+        else lineWidth = 2;
+    
+    }
+    
+
+    function connectColorButtonToListener() {
+        const colors = document.getElementsByClassName("color");
+        console.log(colors);
+        for(let i = 0; i < colors.length ; i++) {
+            console.log(colors[i])
+            colors[i].addEventListener('click', () => paintColor(colors[i]));
+        }
+    }
+
+    
+
+    function init() {
+        canvas = document.getElementById('can');
+
+        ctx = canvas.getContext("2d");
+        w = canvas.width;
+        h = canvas.height;
+    
+        canvas.addEventListener("mousemove", function (e) {
+            findxy('move', e)
+        }, false);
+        canvas.addEventListener("mousedown", function (e) {
+            findxy('down', e)
+        }, false);
+        canvas.addEventListener("mouseup", function (e) {
+            findxy('up', e)
+        }, false);
+        canvas.addEventListener("mouseout", function (e) {
+            findxy('out', e)
+        }, false);
+
+        connectColorButtonToListener();
+    }
+    
+    function draw() {
+        ctx.beginPath();
+        ctx.moveTo(prevX, prevY);
+        ctx.lineTo(curX, curY);
+        ctx.strokeStyle = color;
+        ctx.lineWidth = lineWidth;
+        ctx.stroke();
+        ctx.closePath();
+    }
+    
+    function erase() {
+        const wantToClear = confirm("Want to clear");
+        if (wantToClear) {
+            ctx.clearRect(0, 0, w, h);
+            document.getElementById("canvasimg").style.display = "none";
+        }
+    }
+    
+    function save() {
+        document.getElementById("canvasimg").style.border = "2px solid";
+        var dataURL = canvas.toDataURL();
+        document.getElementById("canvasimg").src = dataURL;
+        document.getElementById("canvasimg").style.display = "inline";
+    }
+    
+    function findxy(res, e) {
+        if (res == 'down') {
+            prevX = curX;
+            prevY = curY;
+            curX = e.clientX - canvas.offsetLeft;
+            curY = e.clientY - canvas.offsetTop;
+    
+            flag = true;
+            dot_flag = true;
+            if (dot_flag) {
+                ctx.beginPath();
+                ctx.fillStyle = color;
+                ctx.fillRect(curX, curY, 2, 2);
+                ctx.closePath();
+                dot_flag = false;
+            }
+        }
+        if (res == 'up' || res == "out") {
+            flag = false;
+        }
+        if (res == 'move') {
+            if (flag) {
+                prevX = curX;
+                prevY = curY;
+                curX = e.clientX - canvas.offsetLeft;
+                curY = e.clientY - canvas.offsetTop;
+                draw();
+            }
+        }
+    }
+
